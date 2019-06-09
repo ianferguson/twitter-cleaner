bf17493d98996bd9b81b2c7dcfb7c158c9aea344
Ian Ferguson <ian@ianferguson.io>
2019-06-09 11:20:56 -0400

    write user timeline crawler function, start to write some v primitive scoring/filtering

--- cleaner.go
+++ cleaner.go
@@ -2,9 +2,12 @@ package twittercleaner
 
 import (
 	"github.com/ChimeraCoder/anaconda"
+	"math"
+	"net/url"
 	"os"
 	"fmt"
 	"errors"
+	"strconv"
 )
 
 type cleaner struct {
@@ -23,7 +26,6 @@ func New() (*cleaner, error) {
 }
 
 func (cleaner *cleaner) getTweets() (*[]anaconda.Tweet, error) {
-
 	search, err := cleaner.client.GetSearch("colonial williamsburg since:2019-06-03 until:2019-06-08", nil)
 	if err != nil {
 		return nil, err
@@ -37,6 +39,73 @@ func (cleaner *cleaner) getTweets() (*[]anaconda.Tweet, error) {
 		fmt.Printf("found %d more tweets\n", len(search.Statuses))
 	}
 	return &tweets, nil
+}
+
+func (cleaner *cleaner) GetMyTweets() (*[]anaconda.Tweet, error) {
+	values := url.Values{
+		"count": {"200"},
+	}
+
+	var tweets []anaconda.Tweet
+	var maxId int64 = math.MaxInt64 - 1
+	for ;; {
+		values.Set("max_id", strconv.FormatInt(maxId, 10))
+		fmt.Printf("Set max_id to %s\n", values.Get("max_id"))
+
+		newTweets, err := cleaner.client.GetUserTimeline(values)
+		if err != nil {
+			return nil, err
+		}
+
+		tweetsFound := len(newTweets)
+		if tweetsFound == 0 {
+			fmt.Printf("No more tweets returned\n")
+			break
+		}
+		fmt.Printf("Found %d tweets\n", tweetsFound)
+
+		for _, tweet := range newTweets {
+			if tweet.Id < maxId {
+				// twitter api docs imply that the max_id query is not inclusive
+				// of max_id, but in practice it appears to be, so we decrement it
+				// down one from our epoch mark
+				maxId = tweet.Id - 1
+			}
+		}
+
+		tweets = append(tweets, newTweets...)
+	}
+
+	return &tweets, nil
+}
+
+func filterUnengagedTweets(tweets *[]anaconda.Tweet) *[]anaconda.Tweet {
+	filtered := make([]anaconda.Tweet, 0, len(*tweets))
+	for _, tweet := range *tweets {
+		if score(tweet) == 0 {
+			filtered = append(filtered, tweet)
+		}
+	}
+	return &filtered
+}
+
+func filterEngagedTweets(tweets *[]anaconda.Tweet) *[]anaconda.Tweet {
+	filtered := make([]anaconda.Tweet, 0, len(*tweets))
+	for _, tweet := range *tweets {
+		if score(tweet) > 0 {
+			filtered = append(filtered, tweet)
+		}
+	}
+	return &filtered
+}
+
+func score(tweet anaconda.Tweet) (score int) {
+	score += tweet.RetweetCount * 2
+	score += tweet.FavoriteCount
+	if tweet.User.ScreenName != "ianwords" {
+		score = -1
+	}
+	return score
 }
 
 func client() (*anaconda.TwitterApi, error) {
--- cleaner_test.go
+++ cleaner_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestThatTweetsCanBeRetrieved(t *testing.T) {
+func TestThatSearchTweetsCanBeRetrieved(t *testing.T) {
 	cleaner, err := New()
 	if err != nil {
 		t.Fatal(err)
@@ -21,6 +21,52 @@ func TestThatTweetsCanBeRetrieved(t *testing.T) {
 	}
 	t.Error("not yet implemented")
 }
+
+func TestThatUserTimelineCanBeRetrieved(t *testing.T) {
+	cleaner, err := New()
+	if err != nil {
+		 t.Fatal(err)
+	}
+
+	tweets, err := cleaner.GetMyTweets()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, tweet := range *tweets {
+		fmt.Printf("%s: %s\n", tweet.User.Name, tweet.Text)
+	}
+
+	fmt.Printf("Found %d total tweets", len(*tweets))
+	t.Error("not yet implemented")
+}
+
+func TestTweetFiltering(t *testing.T) {
+	cleaner, err := New()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tweets, err := cleaner.GetMyTweets()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Printf("Found %d total tweets", len(*tweets))
+
+	unengaged := filterUnengagedTweets(tweets)
+	for _, tweet := range *unengaged {
+		fmt.Printf("delete: %s\n", tweet.Text)
+	}
+
+	engaged := filterEngagedTweets(tweets)
+	for _, tweet := range *engaged {
+		fmt.Printf("keep: %s\n", tweet.Text)
+	}
+
+	t.Error("not yet implemented")
+}
+
 
 func TestThatTweetsOlderThan30DaysWithoutEngagementsAreRemoved(t *testing.T) {
 	t.Error("not yet implemented")
